95174e200bcb8beaa09560d5515de0048dce655a
Caua_Mistura <cauamistura1@gmail.com>
2023-06-12 22:24:15 -0300

    app task 1.1

--- apps/app_tasks/lib/pages/todo_list_page.dart
+++ apps/app_tasks/lib/pages/todo_list_page.dart
@@ -1,20 +1,93 @@
 import 'package:flutter/material.dart';
 
-class TodoListPage extends StatelessWidget {
-  const TodoListPage({super.key});
+class TodoListPage extends StatefulWidget {
+  const TodoListPage({Key? key}) : super(key: key);
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  final TextEditingController _taskController = TextEditingController();
+  List<String> tasks = [];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(30),
-          child: TextField(
-            decoration: InputDecoration(
-              labelText: 'Nova Tarefa',
-              border: OutlineInputBorder(),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Minhas Tarefas'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 4,
+                      child: TextField(
+                        controller: _taskController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Tarefa',
+                          hintText: 'Ex. Nova tarefa',
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Expanded(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.all(16.5),
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            tasks.add(_taskController.text);
+                          });
+                        },
+                        child: const Icon(
+                          Icons.add,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                ListView(
+                  shrinkWrap: true,
+                  children: [
+                    for (String task in tasks)
+                      ListTile(
+                        title: Text(task),
+                        onTap: () {
+                          print('Clicou na tarefa $task');
+                        },
+                      )
+                  ],
+                ),
+              ],
             ),
-          ),
+            Row(
+              children: [
+                Expanded(
+                  child: Text('Você possui ${tasks.length} tarefas pendentes'),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      tasks.clear();
+                    });
+                  },
+                  child: const Text('Limpar Tudo'),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
